318daaab1828df933aebb15c3e8cb9a8bfec891a
Guillermo Julián <gjulianm@outlook.com>
2015-02-28 16:08:45 +0100

    Initial commit

---
+++ heart.sql
@@ -0,0 +1,85 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 192.168.59.103 (MySQL 5.6.23)
+# Database: heart
+# Generation Time: 2015-02-28 12:07:15 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table params
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `params`;
+
+CREATE TABLE `params` (
+  `id` int(11) DEFAULT NULL,
+  `param_name` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table raw_data
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `raw_data`;
+
+CREATE TABLE `raw_data` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `param_t` int(11) DEFAULT NULL,
+  `value` double DEFAULT '0',
+  PRIMARY KEY (`user_id`,`timestamp`, `param_t`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table stats
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `stats`;
+
+CREATE TABLE `stats` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `param_t` int(11) NOT NULL DEFAULT '0',
+  `avg` double DEFAULT NULL,
+  `var` double DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`param_t`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) DEFAULT NULL,
+  `surname` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
